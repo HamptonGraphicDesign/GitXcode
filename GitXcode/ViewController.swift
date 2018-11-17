1b89e7743d2c8d91cf752b2c2867b33263fd94f8
Dameon Johnson <dameonjohnson@dameons-macbook-pro.local>
2018-11-16 21:51:35 -0500

    Initial Commit

---
+++ GitXcode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitXcode
+//
+//  Created by Dameon Johnson on 11/16/18.
+//  Copyright © 2018 Dameon Johnson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
